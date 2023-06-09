2f184cc3df4be9521a682f0d06f23f088a96ab52
ShpriZZ <Dzerenus@ya.ru>
2023-06-09 23:54:45 +0900

    Черновик переаботки окна добавления потока

--- UDPFlood/UDPFlood.Flooder/ViewModels/AddThreadViewModel.cs
+++ UDPFlood/UDPFlood.Flooder/ViewModels/AddThreadViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using UDPFlood.Ethernet.FloodSettings;
@@ -14,177 +15,13 @@ public class AddThreadViewModel : INotifyPropertyChanged
 
     public event AddThreadEventArgs? AddThread;
 
-    public AddThreadInput DestIp { get; }
-
-    public string IPSourceAddressesTextboxText
-    {
-        get => _iPSourceAddressesTextboxText;
-        set
-        {
-            _iPSourceAddressesTextboxText = value;
-            OnPropertyChanged(nameof(IPSourceAddressesTextboxText));
-            ValidateInput();
-        }
-    }
-    public bool IPSourceAddressesTextboxEnabled
-    {
-        get => _iPSourceAddressesTextboxEnabled;
-        set
-        {
-            _iPSourceAddressesTextboxEnabled = value;
-            OnPropertyChanged(nameof(IPSourceAddressesTextboxEnabled));
-        }
-    }
-    public bool IPSourceIsFromTextBox
-    {
-        get => _iPSourceIsFromTextBox;
-        set
-        {
-            IPSourceAddressesTextboxEnabled = value;
-            _iPSourceIsFromTextBox = value;
-            OnPropertyChanged(nameof(IPSourceIsFromTextBox));
-            ValidateInput();
-        }
-    }
-    public bool IPSourceIsRandom
-    {
-        get => _iPSourceIsRandom;
-        set
-        {
-            _iPSourceIsRandom = value;
-            OnPropertyChanged(nameof(IPSourceIsRandom));
-        }
-    }
-    public bool IPSourceIsMine
-    {
-        get => _iPSourceIsMine;
-        set
-        {
-            _iPSourceIsMine = value;
-            OnPropertyChanged(nameof(IPSourceIsMine));
-        }
-    }
-
-    public string MACSourceAddressesTextboxText
-    {
-        get => _macSourceAddressesTextboxText;
-        set
-        {
-            _macSourceAddressesTextboxText = value;
-            OnPropertyChanged(nameof(MACSourceAddressesTextboxText));
-            ValidateInput();
-        }
-    }
-    public bool MACSourceAddressesTextboxEnabled
-    {
-        get => _macSourceAddressesTextboxEnabled;
-        set
-        {
-            _macSourceAddressesTextboxEnabled = value;
-            OnPropertyChanged(nameof(MACSourceAddressesTextboxEnabled));
-        }
-    }
-    public bool MACSourceIsFromTextBox
-    {
-        get => _macSourceIsFromTextBox;
-        set
-        {
-            MACSourceAddressesTextboxEnabled = value;
-            _macSourceIsFromTextBox = value;
-            OnPropertyChanged(nameof(MACSourceIsFromTextBox));
-            ValidateInput();
-        }
-    }
-    public bool MACSourceIsRandom
-    {
-        get => _macSourceIsRandom;
-        set
-        {
-            _macSourceIsRandom = value;
-            OnPropertyChanged(nameof(MACSourceIsRandom));
-        }
-    }
-    public bool MACSourceIsMine
-    {
-        get => _macSourceIsMine;
-        set
-        {
-            _macSourceIsMine = value;
-            OnPropertyChanged(nameof(MACSourceIsMine));
-        }
-    }
-
-    public string PortsDistinationTextboxText
-    {
-        get => _portsDistinationTextboxText;
-        set
-        {
-            _portsDistinationTextboxText = value;
-            OnPropertyChanged(nameof(PortsDistinationTextboxText));
-            ValidateInput();
-        }
-    }
-    public bool PortsDistinationTextboxEnabled
-    {
-        get => _portsDistinationTextboxEnabled;
-        set
-        {
-            _portsDistinationTextboxEnabled = value;
-            OnPropertyChanged(nameof(PortsDistinationTextboxEnabled));
-        }
-    }
-    public bool PortsDistinationIsFromTextBox
-    {
-        get => _portDistinationIsFromTextBox;
-        set
-        {
-            PortsDistinationTextboxEnabled = value;
-            _portDistinationIsFromTextBox = value;
-            OnPropertyChanged(nameof(PortsDistinationIsFromTextBox));
-            ValidateInput();
-        }
-    }
-    public bool PortsDistinationIsRandom
-    {
-        get => _portDistinationIsRandom;
-        set
-        {
-            _portDistinationIsRandom = value;
-            OnPropertyChanged(nameof(PortsDistinationIsRandom));
-        }
-    }
-
-    public string SourcePortTextboxText
-    {
-        get => _sourcePortTextboxText;
-        set
-        {
-            _sourcePortTextboxText = value;
-            OnPropertyChanged(nameof(SourcePortTextboxText));
-            ValidateInput();
-        }
-    }
-
-    public bool SourcePortIsRandom
-    {
-        get => _sourcePortIsRandom;
-        set
-        {
-            _sourcePortIsRandom = value;
-            OnPropertyChanged(nameof(SourcePortIsRandom));
-        }
-    }
-
-    public bool SourcePortIsFromTextbox
-    {
-        get => _sourcePortIsFromTextbox;
-        set
-        {
-            _sourcePortIsFromTextbox = value;
-            OnPropertyChanged(nameof(SourcePortIsFromTextbox));
-            ValidateInput();
-        }
-    }
+    public AddThreadInput DestinationIpField { get; }
+    public AddThreadInput SourceIpField { get; }
+    public AddThreadInput DestinationMacField { get; }
+    public AddThreadInput SourceMacField { get; }
+    public AddThreadInput DestinationPortField { get; }
+    public AddThreadInput SourcePortField { get; }
+    public AddThreadInput ContentField { get; }
 
     public bool TimeIsDelayEnabled
     {
@@ -236,141 +73,131 @@ public class AddThreadViewModel : INotifyPropertyChanged
         }
     }
 
-    public bool ContentIsEmptyMessage
-    {
-        get => _contentIsEmptyMessage;
-        set
-        {
-            if (value)
-            {
-                ContentIsRandomMessage = false;
-                ContentIsTextBoxMessage = false;
-            }
-
-            _contentIsEmptyMessage = value;
-            OnPropertyChanged(nameof(ContentIsEmptyMessage));
-        }
-    }
-    public bool ContentIsRandomMessage
-    {
-        get => _contentIsRandomMessage;
-        set
-        {
-            if (value)
-            {
-                ContentIsEmptyMessage = false;
-                ContentIsTextBoxMessage = false;
-            }
-
-            _contentIsRandomMessage = value;
-            OnPropertyChanged(nameof(ContentIsRandomMessage));
-        }
-    }
-    public bool ContentIsTextBoxMessage
-    {
-        get => _contentIsTextboxMessage;
-        set
-        {
-            if (value)
-            {
-                ContentIsRandomMessage = false;
-                ContentIsEmptyMessage = false;
-            }
-
-            _contentIsTextboxMessage = value;
-            OnPropertyChanged(nameof(ContentIsTextBoxMessage));
-            ValidateInput();
-        }
-    }
-    public string ContentTextBoxText
-    {
-        get => _contentTextboxText;
-        set
-        {
-            _contentTextboxText = value;
-            OnPropertyChanged(nameof(ContentTextBoxText));
-            ValidateInput();
-        }
-    }
-
-    public string ErrorMessage
-    {
-        get => _errorMessage;
-        set
-        {
-            _errorMessage = value;
-            ErrorMessageVisibility = value == string.Empty ? Visibility.Hidden : Visibility.Visible;
-            IsInputValid = value == string.Empty;
-
-            OnPropertyChanged(nameof(ErrorMessage));
-            OnPropertyChanged(nameof(ErrorMessageVisibility));
-            OnPropertyChanged(nameof(IsInputValid));
-        }
-    }
-    public Visibility ErrorMessageVisibility { get; set; } = Visibility.Hidden;
-    public bool IsInputValid { get; set; }
-
     public ICommand AddThreadCommand { get; private set; }
-
-    private string _errorMessage = string.Empty;
-
-    private bool _contentIsEmptyMessage = true;
-    private bool _contentIsRandomMessage = true;
-    private bool _contentIsTextboxMessage = true;
-    private string _contentTextboxText = string.Empty;
 
     private bool _timeIsDelayEnabled = false;
     private string _timeEverySecondsTextboxText = "60";
     private string _timeLengthTextboxText = "15";
     private string _timeDelayTextboxText = "5";
 
-    private string _sourcePortTextboxText = string.Empty;
-    private bool _sourcePortIsRandom = true;
-    private bool _sourcePortIsFromTextbox = false;
-
-    private string _portsDistinationTextboxText = string.Empty;
-    private bool _portsDistinationTextboxEnabled = false;
-    private bool _portDistinationIsFromTextBox = false;
-    private bool _portDistinationIsRandom = true;
-
-    private string _macSourceAddressesTextboxText = string.Empty;
-    private bool _macSourceAddressesTextboxEnabled = false;
-    private bool _macSourceIsFromTextBox = true;
-    private bool _macSourceIsRandom = true;
-    private bool _macSourceIsMine = true;
-
-    private string _iPSourceAddressesTextboxText = string.Empty;
-    private bool _iPSourceAddressesTextboxEnabled = false;
-    private bool _iPSourceIsFromTextBox = true;
-    private bool _iPSourceIsRandom = true;
-    private bool _iPSourceIsMine = true;
-
     public AddThreadViewModel()
     {
         var isIpInputValid = new Predicate<string>(x => x.Split(';').All(s => Ethernet.Validate.IsValidIP(s) && s.Length > 0));
+        var isMacInputValid = new Predicate<string>(x => x.Split(';').All(s => Ethernet.Validate.IsValidMAC(s) && s.Length > 0));
+        var isPortInputValid = new Predicate<string>(x => x.Split(';').All(x => ushort.TryParse(x, out _)));
+        var isContentValid = new Predicate<string>(x => Encoding.UTF8.GetBytes(x).Length <= 508);
 
         var destIpComboBoxItems = new List<ComboBoxItem>() 
         {
-            new("Случайный IP назначения для каждого пакета", 0),
-            new("Выбор из списка", 1)
+            new("Случайный для каждого пакета", (int)IpMode.Random),
+            new("Случайный из списка", (int)IpMode.FromList)
         };
 
-        DestIp = new(destIpComboBoxItems, destIpComboBoxItems[^1], isIpInputValid);
+        var srcIpComboBoxItems = new List<ComboBoxItem>()
+        {
+            new("Случайный для каждого пакета", (int)IpMode.Random),
+            new("Использовать адрес сетевой карты", (int)IpMode.MySelf),
+            new("Случайный из списка", (int)IpMode.FromList)
+        };
 
+        var destMacComboBoxItems = new List<ComboBoxItem>()
+        {
+            new("Случайный для каждого пакета", (int)MacMode.FromList),
+            new("Случайный из списка", (int)MacMode.Random)
+        };
+
+        var srcMacComboBoxItems = new List<ComboBoxItem>()
+        {
+            new("Случайный для каждого пакета", (int)MacMode.Random),
+            new("Использовать адрес сетевой карты", (int)MacMode.MySelf),
+            new("Случайный из списка", (int)MacMode.FromList)
+        };
+
+        var portComboBoxItems = new List<ComboBoxItem>()
+        {
+            new("Случайный для каждого пакета", (int)PortMode.FromList),
+            new("Случайный из списка", (int)PortMode.Random)
+        };
+
+        var contentComboBoxItems = new List<ComboBoxItem>()
+        {
+            new("Без данных", (int)ContentMode.Empty),
+            new("Случайные данные", (int)ContentMode.Random),
+            new("Ввести данные (UTF-8)", (int)ContentMode.FromContent)
+        };
+
+        DestinationIpField = new(destIpComboBoxItems, destIpComboBoxItems[^1], isIpInputValid);
+        SourceIpField = new(srcIpComboBoxItems, srcIpComboBoxItems[^1], isIpInputValid);
+        DestinationMacField = new(destMacComboBoxItems, destIpComboBoxItems[^1], isMacInputValid);
+        SourceMacField = new(srcMacComboBoxItems, destIpComboBoxItems[^1], isMacInputValid);
+        DestinationPortField = new(portComboBoxItems, portComboBoxItems[^1], isPortInputValid);
+        SourcePortField = new(portComboBoxItems, portComboBoxItems[^1], isPortInputValid);
+        ContentField = new(contentComboBoxItems, contentComboBoxItems[^1], isContentValid);
 
         AddThreadCommand = new RelayCommand(x => DoAddThread());
     }
 
     private void DoAddThread()
     {
-        if (!IsInputValid)
-            throw new InvalidOperationException();
+        if (SourceIpField.IsError)
+        {
+            MessageBox.Show("IP источника задан неверно", "Ошибка!");
+            return;
+        }
 
-        var ipSettings = IPSourceIsMine ? new IpSettings(IpMode.MySelf)
-            : IPSourceIsRandom ? new IpSettings(IpMode.Random)
-            : IPSourceIsFromTextBox ? new IpSettings(IpMode.FromList, IPSourceAddressesTextboxText.Split(";"))
-            : throw new InvalidOperationException();
+        if (DestinationIpField.IsError)
+        {
+            MessageBox.Show("IP назначения задан неверно", "Ошибка!");
+            return;
+        }
 
+        if (SourceMacField.IsError)
+        {
+            MessageBox.Show("MAC источника задан неверно", "Ошибка!");
+            return;
+        }
+
+        if (DestinationMacField.IsError)
+        {
+            MessageBox.Show("MAC назначения задан неверно", "Ошибка!");
+            return;
+        }
+
+        if (SourcePortField.IsError)
+        {
+            MessageBox.Show("Порт источника задан неверно", "Ошибка!");
+            return;
+        }
+
+        if (DestinationPortField.IsError)
+        {
+            MessageBox.Show("Порт назначения задан неверно", "Ошибка!");
+            return;
+        }
+
+        if (ContentField.IsError)
+        {
+            MessageBox.Show("Содержимое пакета задано неверно", "Ошибка!");
+            return;
+        }
+
+
+        var srcIpMode = (IpMode)SourceIpField.SelectedItem.Value;
+        var dstIpMode = (IpMode)DestinationIpField.SelectedItem.Value;
+
+        var srcMacMode = (MacMode)SourceMacField.SelectedItem.Value;
+        var dstMacMode = (MacMode)DestinationMacField.SelectedItem.Value;
+
+        var srcPortMode = (PortMode)SourcePortField.SelectedItem.Value;
+        var dstPortMode = (PortMode)DestinationPortField.SelectedItem.Value;
+
+        var contentMode = (ContentMode)ContentField.SelectedItem.Value;
+
+        var srcIp = new IpSettings(srcIpMode, srcIpMode == IpMode.FromList ? SourceIpField.TextBoxValue.Split(';') : null);
+        var dstIp = new IpSettings(dstIpMode, dstIpMode == IpMode.FromList ? DestinationIpField.TextBoxValue.Split(';') : null);
+
+        var srcMac = new MacSettings(srcMacMode, srcMacMode == MacMode.FromList ? SourceMacField.TextBoxValue.Split(';') : null);
+        var dstMac = new MacSettings(srcMacMode, srcMacMode == MacMode.FromList ? SourceMacField.TextBoxValue.Split(';') : null);
 
         var macSettings = MACSourceIsMine ? new MacSettings(MacMode.MySelf)
             : MACSourceIsRandom ? new MacSettings(MacMode.Random)
@@ -395,76 +222,6 @@ public class AddThreadViewModel : INotifyPropertyChanged
             : throw new InvalidOperationException();
 
         AddThread?.Invoke(new(contentSettings, delaySettings, ipSettings, portSettings, srcPortSettings, macSettings));
-    }
-
-    private void ValidateInput()
-    {
-        if (DestIp.IsError)
-        {
-            ErrorMessage = "Неверно заданы IP назначения!";
-            return;
-        }
-
-        if (IPSourceIsFromTextBox)
-        {
-            var parts = IPSourceAddressesTextboxText.Split(";");
-
-            foreach (var part in parts)
-                if (!Ethernet.Validate.IsValidIP(part))
-                {
-                    ErrorMessage = "Неверно заданы IP источника!";
-                    return;
-                }
-        }
-
-        if (MACSourceIsFromTextBox)
-        {
-            var parts = MACSourceAddressesTextboxText.Split(";");
-
-            foreach (var part in parts)
-                if (!Ethernet.Validate.IsValidMAC(part))
-                {
-                    ErrorMessage = "Неверно заданы MAC источника!";
-                    return;
-                }
-        }
-
-        if (SourcePortIsFromTextbox)
-        {
-            var parts = PortsDistinationTextboxText.Split(";");
-
-            foreach (var part in parts)
-                if (!Ethernet.Validate.IsValidPort(part))
-                {
-                    ErrorMessage = "Неверно заданы порты источника!";
-                    return;
-                }
-        }
-
-        if (PortsDistinationIsFromTextBox)
-        {
-            var parts = PortsDistinationTextboxText.Split(";");
-
-            foreach (var part in parts)
-                if (!Ethernet.Validate.IsValidPort(part))
-                {
-                    ErrorMessage = "Неверно заданы порты назначения!";
-                    return;
-                }
-        }
-
-        if (TimeIsDelayEnabled)
-        {
-            if (!int.TryParse(TimeEverySecondsTextboxText, out var everySecond) || everySecond < 0
-                || !int.TryParse(TimeLengthTextboxText, out var timeLength) || timeLength < 0 || timeLength > everySecond
-                || !int.TryParse(TimeDelayTextboxText, out var timeDelay) || timeDelay < 0 || timeDelay >= timeLength)
-            {
-                ErrorMessage = "Время задано неверно!";
-                return;
-            }
-        }
-
-        ErrorMessage = string.Empty;
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
